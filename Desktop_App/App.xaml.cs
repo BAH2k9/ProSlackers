75a560621bb5836ef440c0be566c3ca787c1101b
bailey halliday <you@example.com>
2024-06-04 23:36:37 +0100

    Small refactor

--- Desktop_App/App.xaml.cs
+++ Desktop_App/App.xaml.cs
@@ -8,6 +8,7 @@ namespace Desktop_App
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    /// https://www.swiss-slackline.ch/en/what-is-slacklining/
     public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
